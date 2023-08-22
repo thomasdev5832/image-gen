e6c18f668706f23fd4a229acfc533677f76a7e23
Gabriel Thomas <gabriel.alphon@gmail.com>
2023-08-22 16:44:48 -0300

    update ai generate

--- server/routes/imageGenRoutes.js
+++ server/routes/imageGenRoutes.js
@@ -14,24 +14,23 @@ router.route('/').get((req, res) => {
     res.send('Hello from OpenAI!');
 });
 
-router.route('/').post( async (req, res) => {
+router.route('/').post(async (req, res) => {
     try {
-        const { prompt } = req.body;
-
-        const aiResponse = await openai.createImage({
-            prompt,
-            n: 1,
-            size: '1024x1024',
-            response_format: 'b64_json',
-        });
-
-        const image = aiResponse.data.data[0].b64_json;
-
-        res.status(200).json({ photo: image });
+      const { prompt } = req.body;
+  
+      const aiResponse = await openai.createImage({
+        prompt,
+        n: 1,
+        size: '1024x1024',
+        response_format: 'b64_json',
+      });
+  
+      const image = aiResponse.data.data[0].b64_json;
+      res.status(200).json({ photo: image });
     } catch (error) {
-        console.log(error);
-        res.status(500).send(error?.response.data.error.message);
+      console.error(error);
+      res.status(500).send(error?.response.data.error.message || 'Something went wrong');
     }
-});
-
-export default router;
+  });
+  
+  export default router;
